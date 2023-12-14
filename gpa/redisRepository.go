2ce93494e38626297fb13322e5a22e65b63cf5c5
thftgr <thftgr@gmail.com>
2023-12-14 16:51:51 +0900

    redisRepository 구현

--- gpa/redisRepository.go
+++ gpa/redisRepository.go
@@ -1,19 +1,90 @@
 package gpa
 
+import (
+	"context"
+	"github.com/redis/go-redis/v9"
+)
+
 type RedisEntityId interface {
 	Id
+	ToString() string // redis key 가 string 임.
 }
 
 type RedisEntity[ID RedisEntityId] interface {
 	Entity[ID]
-	KeyPrefix() string
+
+	// 필요한 경우 아래 두 인터페이스를 구현할것.
+	//json.Marshaler
+	//json.Unmarshaler
+
+	// entity struct 구현시 redis 태그 사용
+	// UserId int `redis:"userId"`
+	// Email  int `redis:"email"`
 }
 
 type RedisRepository[E RedisEntity[ID], ID RedisEntityId] interface {
-	//CrudRepository[E, ID]
+	CrudRepository[E, ID]
 }
 
-//type RedisRepositoryImpl[E RedisEntity[ID], ID RedisEntityId] struct {
-//	Model E             // 제너릭을 통해 자동 설정됨.
-//	DB    *redis.Client // 필수로 추가해야함
-//}
+type RedisRepositoryImpl[E RedisEntity[ID], ID RedisEntityId] struct {
+	ctx   context.Context
+	Model E             // 제너릭을 통해 자동 설정됨.
+	DB    *redis.Client // 필수로 추가해야함
+}
+
+func (r *RedisRepositoryImpl[E, ID]) Save(e E) error {
+	return r.DB.HMSet(r.ctx, e.GetId().ToString(), e).Err()
+}
+
+func (r *RedisRepositoryImpl[E, ID]) SaveAll(e ...E) (count int64, err error) {
+	for i := range e {
+		err = r.Save(e[i])
+		if err != nil {
+			return
+		}
+		count++
+	}
+	return
+}
+
+func (r *RedisRepositoryImpl[E, ID]) FindById(id ID) (e E, err error) {
+	err = r.DB.HMGet(r.ctx, id.ToString()).Scan(&e)
+	return
+}
+
+func (r *RedisRepositoryImpl[E, ID]) FindAllById(id ...ID) (res []E, err error) {
+	for i := range id {
+		e, err2 := r.FindById(id[i])
+		if err2 != nil {
+			err = err2
+			return
+		}
+		res = append(res, e)
+	}
+	return
+}
+
+func (r *RedisRepositoryImpl[E, ID]) Delete(e E) error {
+	return r.DB.Del(r.ctx, e.GetId().ToString()).Err()
+}
+
+func (r *RedisRepositoryImpl[E, ID]) DeleteAll(e ...E) (count int64, err error) {
+	ids := make([]string, len(e))
+	for i := range e {
+		ids[i] = e[i].GetId().ToString()
+	}
+	return r.DB.Del(r.ctx, ids...).Result()
+
+}
+
+func (r *RedisRepositoryImpl[E, ID]) DeleteById(id ID) error {
+	return r.DB.Del(r.ctx, id.ToString()).Err()
+}
+
+func (r *RedisRepositoryImpl[E, ID]) DeleteAllById(id ...ID) (count int64, err error) {
+	ids := make([]string, len(id))
+	for i := range id {
+		ids[i] = id[i].ToString()
+	}
+	return r.DB.Del(r.ctx, ids...).Result()
+}
