e26d623fc6afc28d533cbd0493a05e3d3b939d36
thftgr <thftgr@gmail.com>
2023-12-25 21:47:40 +0900

    불필요 코드 제거

--- gpa/constant.go
+++
@@ -1,9 +0,0 @@
-package gpa
-
-import (
-	"github.com/thftgr/go-utils/gpa/influxRepository"
-	"reflect"
-)
-
-var INFLUX_ENTITY_ENCODER_TYPE = reflect.TypeOf((*influxRepository.InfluxEntityEncoder)(nil)).Elem()
-var INFLUX_ENTITY_DECODER_TYPE = reflect.TypeOf((*influxRepository.InfluxEntityDecoder)(nil)).Elem()
