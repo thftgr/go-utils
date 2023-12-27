e77afd6c2b7f88ffb7481e226b66e490cf01fac8
thftgr <thftgr@gmail.com>
2023-12-27 18:02:24 +0900

    influxRepository delete 테스트코드 작성1

--- gpa/influxRepository/influxRepository_test.go
+++ gpa/influxRepository/influxRepository_test.go
@@ -25,10 +25,10 @@ func (r RepositoryTestEntity) GetTime() time.Time {
 }
 
 type RepositoryTestEntityRepository struct {
-	InfluxRepository[RepositoryTestEntity]
+	*InfluxRepositoryImpl[RepositoryTestEntity]
 }
 
-func TestInfluxRepositoryImpl_FullTest(t *testing.T) {
+func TestInfluxRepositoryImpl_FullFindTest(t *testing.T) {
 	INFLUXDB_TOKEN := os.Getenv("INFLUXDB_TOKEN")
 	INFLUXDB_BUCKET := os.Getenv("INFLUXDB_BUCKET")
 	INFLUXDB_ORG := os.Getenv("INFLUXDB_ORG")
@@ -41,7 +41,7 @@ func TestInfluxRepositoryImpl_FullTest(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
 	repo := &RepositoryTestEntityRepository{
-		InfluxRepository: NewInfluxRepositoryImpl[RepositoryTestEntity](
+		InfluxRepositoryImpl: NewInfluxRepositoryImpl[RepositoryTestEntity](
 			INFLUXDB_ORG, INFLUXDB_BUCKET, influxdb2.NewClient(INFLUXDB_URL, INFLUXDB_TOKEN), ctx, time.Second*5,
 		),
 	}
@@ -60,7 +60,8 @@ func TestInfluxRepositoryImpl_FullTest(t *testing.T) {
 	}
 	utils.ReverseSlice[RepositoryTestEntity](testEntities)
 	var startT, stopT = now.Add(-(time.Second * 5)), now.Add(time.Second)
-	var startD, stopD = now.Sub(now.Add(-(time.Second * 5))), time.Second
+	var startD, stopD = now.Sub(now.Add(-(time.Second * 5))), time.Duration(0)
+	t.Logf("now: %+v, startT: %+v, stopT: %+v, startD: %+v, stopD: %+v", now, startT, stopT, startD, stopD)
 
 	if e, err := repo.FindAllByTime(startT, stopT); err != nil { // -5s ~ now
 		t.Error(err)
@@ -70,8 +71,8 @@ func TestInfluxRepositoryImpl_FullTest(t *testing.T) {
 
 	if e, err := repo.FindAllByDuration(startD, stopD); err != nil { // -5s ~ now
 		t.Error(err)
-	} else if !reflect.DeepEqual(e, testEntities[:1]) {
-		t.Errorf("FindAllByDuration() gotRes = %v, want %v", e, testEntities[:1])
+	} else if !reflect.DeepEqual(e, testEntities) {
+		t.Errorf("FindAllByDuration() gotRes = %v, want %v", e, testEntities)
 	}
 
 	if e, err := repo.FindAllByTagsAndTime(startT, stopT, testTags); err != nil {
@@ -82,32 +83,99 @@ func TestInfluxRepositoryImpl_FullTest(t *testing.T) {
 
 	if e, err := repo.FindAllByTagsAndDuration(startD, stopD, testTags); err != nil {
 		t.Error(err)
-	} else if !reflect.DeepEqual(e, testEntities[:1]) {
-		t.Errorf("FindAllByTagsAndDuration() gotRes = %v, want %v", e, testEntities[:1])
+	} else if !reflect.DeepEqual(e, testEntities) {
+		t.Errorf("FindAllByTagsAndDuration() gotRes = %v, want %v", e, testEntities)
 	}
 
-	if e, err := repo.FindAllByTimeAfter(startT); err != nil {
+	if err := repo.DeleteByTime(startT, stopT, nil); err != nil {
 		t.Error(err)
-	} else if !reflect.DeepEqual(e, testEntities) {
-		t.Errorf("FindAllByTimeAfter() gotRes = %v, want %v", e, testEntities)
 	}
 
-	if e, err := repo.FindAllByDurationAfter(startD); err != nil {
-		t.Error(err)
-	} else if !reflect.DeepEqual(e, testEntities) {
-		t.Errorf("FindAllByDurationAfter() gotRes = %v, want %v", e, testEntities)
+}
+
+func TestInfluxRepositoryImpl_FullDeleteTest(t *testing.T) {
+	INFLUXDB_TOKEN := os.Getenv("INFLUXDB_TOKEN")
+	INFLUXDB_BUCKET := os.Getenv("INFLUXDB_BUCKET")
+	INFLUXDB_ORG := os.Getenv("INFLUXDB_ORG")
+	INFLUXDB_URL := os.Getenv("INFLUXDB_URL")
+
+	//t.Logf("INFLUXDB_TOKEN:  %s", INFLUXDB_TOKEN)
+	t.Logf("INFLUXDB_BUCKET: %s", INFLUXDB_BUCKET)
+	t.Logf("INFLUXDB_ORG:    %s", INFLUXDB_ORG)
+	t.Logf("INFLUXDB_URL:    %s", INFLUXDB_URL)
+	now := time.Now().UTC()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*60)
+	defer cancel()
+	repo := &RepositoryTestEntityRepository{
+		InfluxRepositoryImpl: NewInfluxRepositoryImpl[RepositoryTestEntity](
+			INFLUXDB_ORG, INFLUXDB_BUCKET, influxdb2.NewClient(INFLUXDB_URL, INFLUXDB_TOKEN), ctx, time.Second*600,
+		),
+	}
+	var testTags = []*protocol.Tag{
+		{Key: "name", Value: "test-name"},
+	}
+	var testEntities = []RepositoryTestEntity{
+		{Name: "test-name", LogString: "log string-5s", Status: http.StatusOK, Time: now.Add(-(time.Second * 5))},
+		{Name: "test-name", LogString: "log string+0s", Status: http.StatusOK, Time: now},
+		{Name: "test-name", LogString: "log string+5s", Status: http.StatusOK, Time: now.Add(time.Second * 5)},
 	}
 
-	if e, err := repo.FindAllByTagsAndTimeAfter(startT, testTags); err != nil {
+	var startT, stopT = now.Add(-(time.Second * 5)), now.Add(time.Second)
+	var startD, stopD = now.Sub(now.Add(-(time.Second * 5))), time.Duration(0)
+	_, _, _, _, _ = startT, stopT, startD, stopD, testTags
+
+	for i := range testEntities {
+		if err := repo.SaveAndFlush(testEntities[i]); err != nil {
+			t.Error(err)
+		}
+	}
+	if err := repo.DeleteAllByTime(startT, stopT); err != nil {
 		t.Error(err)
-	} else if !reflect.DeepEqual(e, testEntities) {
-		t.Errorf("FindAllByTagsAndTimeAfter() gotRes = %v, want %v", e, testEntities)
+	} else {
+		if e, err := repo.FindAllByTime(startT, stopT); err != nil {
+			t.Error(err)
+		} else if len(e) > 0 {
+			t.Errorf("FindAllByTagsAndTimeAfter() gotRes = %v, want = []", e)
+		}
+	}
+	if err := repo.DeleteByTime(now.Add(-(time.Second * 6)), now.Add(time.Second*6), nil); err != nil {
+		t.Error(err)
 	}
 
-	if e, err := repo.FindAllByTagsAndDurationAfter(startD, testTags); err != nil {
+	for i := range testEntities {
+		if err := repo.SaveAndFlush(testEntities[i]); err != nil {
+			t.Error(err)
+		}
+	}
+	if err := repo.DeleteAllByTime(now, stopT); err != nil {
 		t.Error(err)
-	} else if !reflect.DeepEqual(e, testEntities) {
-		t.Errorf("FindAllByTagsAndDurationAfter() gotRes = %v, want %v", e, testEntities)
+	} else {
+		if e, err := repo.FindAllByTime(startT, stopT); err != nil {
+			t.Error(err)
+		} else if len(e) != 1 {
+			t.Errorf("FindAllByTagsAndTimeAfter() gotRes = %v, want = []", e)
+		}
+	}
+	if err := repo.DeleteByTime(now.Add(-(time.Second * 6)), now.Add(time.Second*6), nil); err != nil {
+		t.Error(err)
+	}
+
+	for i := range testEntities {
+		if err := repo.SaveAndFlush(testEntities[i]); err != nil {
+			t.Error(err)
+		}
+	}
+	if err := repo.DeleteAllByDuration(startD, stopD); err != nil {
+		t.Error(err)
+	} else {
+		if e, err := repo.FindAllByDuration(startD, stopD); err != nil {
+			t.Error(err)
+		} else if len(e) > 0 {
+			t.Errorf("FindAllByTagsAndTimeAfter() gotRes = %v, want = []", e)
+		}
+	}
+	if err := repo.DeleteByTime(now.Add(-(time.Second * 6)), now.Add(time.Second*6), nil); err != nil {
+		t.Error(err)
 	}
 
 }
