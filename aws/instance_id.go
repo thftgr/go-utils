4a095751e63d810340a9ebfaebc4fbdc116314f5
thftgr <thftgr@gmail.com>
2024-01-18 11:18:36 +0900

    add instance_id getter

---
+++ aws/instance_id.go
@@ -0,0 +1,25 @@
+package aws
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+// GetInstanceId it work in aws ec2
+func GetInstanceId() (string, error) {
+	client := http.Client{Timeout: time.Second}
+	res, err := client.Get("http://169.254.169.254/latest/meta-data/instance-id")
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+	if res.StatusCode == 200 {
+		b, err := io.ReadAll(res.Body)
+		return string(b), err
+	} else {
+		return "", fmt.Errorf("StatusCode != 200, response: %s")
+	}
+
+}
