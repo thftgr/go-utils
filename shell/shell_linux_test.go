ac66c88cccae5be38313a8c3c922e2c6009d26e6
thftgr <thftgr@gmail.com>
2024-02-10 22:13:59 +0900

    wsl ubuntu 에서 테스트 완료

--- shell/shell_linux_test.go
+++ shell/shell_linux_test.go
@@ -1,1 +1,48 @@
 package shell
+
+import (
+	"testing"
+	"time"
+)
+
+func TestBash(t *testing.T) {
+	tests := []struct {
+		name    string
+		command []string
+		wantErr bool
+	}{
+		{"", []string{"ping", "-c", "4", "8.8.8.8"}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotStdout, gotStderr, err := Bash(tt.command...)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Bash() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			t.Logf("Bash() gotStdout = %v", gotStdout)
+			t.Logf("Bash() gotStderr = %v", gotStderr)
+		})
+	}
+}
+
+func TestBashWithTimeout(t *testing.T) {
+	tests := []struct {
+		name    string
+		timeout time.Duration
+		command []string
+		wantErr bool
+	}{
+		{"", time.Second * 30, []string{"ping", "-c", "4", "8.8.8.8"}, false},
+		{"", time.Millisecond, []string{"ping", "-c", "4", "8.8.8.8"}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotStdout, gotStderr, err := BashWithTimeout(tt.timeout, tt.command...)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("BashWithTimeout() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			t.Logf("BashWithTimeout() gotStdout = %v", gotStdout)
+			t.Logf("BashWithTimeout() gotStderr = %v", gotStderr)
+		})
+	}
+}
